73c75ba8eee13c8177900d96b1a0c6c10d75a15a
Xinyi Zhu <juliezhu212@outlook.com>
2022-05-27 15:07:04 -0500

    Add alert

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    var tip = 0.1
-
+    var eachPerson: Double?
+    var tipPercentage = 0.1
+    var numberOfPeople = 2.0
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
-
+    
     @IBAction func tipChanged(_ sender: UIButton) {
         // Deselect all buttons
         zeroPctButton.isSelected = false
@@ -30,21 +32,50 @@ class CalculatorViewController: UIViewController {
         // Change tip by sender button
         switch sender {
         case zeroPctButton:
-            tip = 0.0
+            tipPercentage = 0
         case tenPctButton:
-            tip = 0.1
+            tipPercentage = 0.1
         case twentyPctButton:
-            tip = 0.2
+            tipPercentage = 0.2
         default:
             break
         }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
+        numberOfPeople = sender.value
+        splitNumberLabel.text = String(format: "%.0f", numberOfPeople)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(tip)
+        if let total = Double(billTextField.text!) {
+            let bill = total * (1 + tipPercentage) / numberOfPeople
+            eachPerson = round(bill * 100) / 100
+            performSegue(withIdentifier: "goToResultsSegue", sender: self)
+        } else {
+            let alert = UIAlertController(title: "Alert",
+                                          message: "Invalid bill total",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResultsSegue" {
+            let resultsVC = segue.destination as! ResultsViewController
+            resultsVC.eachPerson = eachPerson
+            resultsVC.tipPercentage = Int(tipPercentage * 100)
+            resultsVC.numberOfPeople = Int(numberOfPeople)
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
+    var eachPerson: Double?
+    var tipPercentage: Int?
+    var numberOfPeople: Int?
+    
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
@@ -17,19 +21,18 @@ class ResultsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        totalLabel.text = String(format: "%.2f", eachPerson ?? 0)
+        settingsLabel.text = "Split between \(numberOfPeople ?? 0) people, with \(tipPercentage ?? 0)% tip."
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
-        
+        dismiss(animated: true, completion: nil)
     }
     
     /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+    
     */
 }
